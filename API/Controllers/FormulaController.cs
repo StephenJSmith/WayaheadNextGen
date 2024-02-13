e1a4b6760952d0134fa4898f7239989144571e2a
Stephen <stephen.smith@wayaheadsystems.com>
2024-02-13 14:17:59 +1100

    Windows or Mac pathfile 901020.xml

--- API/Controllers/FormulaController.cs
+++ API/Controllers/FormulaController.cs
@@ -5,17 +5,20 @@ namespace API.Controllers;
 
 public class FormulaController : BaseApiController
 {
-    // private const string TestPathFile =  @"d:\_projects\Wayahead\WayaheadNextGen\Formulae/901020.xml";
-    private const string TestPathFile =  @"/Users/macmini/Documents/_projects/Wayahead/WayaheadNextGen/Formulae/901020.xml";
-    private const string TestFormulaName = "901020";
-    private const int TestEdition = 1;
-    private const int TestRevision = 1;
+  // private const string TestPathFile =  @"d:\_projects\Wayahead\WayaheadNextGen\Formulae/901020.xml";
+  private string TestPathFile => OperatingSystem.IsWindows()
+    ? @"d:\_projects\Wayahead\WayaheadNextGen\Formulae/901020.xml"
+    : @"/Users/macmini/Documents/_projects/Wayahead/WayaheadNextGen/Formulae/901020.xml";
+  private const string TestFormulaName = "901020";
+  private const int TestEdition = 1;
+  private const int TestRevision = 1;
 
   // TODO: Start with single formula - then supply name, edition, version
   [HttpGet]
   public async Task<IActionResult> GetFormula()
   {
-    return HandleResult(await Mediator.Send(new DetailsHandler.Query{
+    return HandleResult(await Mediator.Send(new DetailsHandler.Query
+    {
       FormulaName = TestFormulaName,
       Edition = TestEdition,
       Revision = TestRevision,
